ddc3c4024edcafefd9841be8640a40be24f19887
foo <katie@foo.(none)>
2013-02-07 20:35:52 +0000

    foo

--- flashcards.js
+++ flashcards.js
@@ -1,12 +1,14 @@
-var numCards = qs.length;
-var maxIndex = numCards-1;
+// Global vars OF JOY!
 var cardIndex = 0;
+var qs;
+var numCards;
+var maxIndex;
+var isFlipped = 0;
 
 // Returns a random integer between minInt and maxInt
 function getRandomInt (minInt, maxInt) {
     return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
 }
-
 function advanceCardIndex() {
     if (cardIndex == maxIndex) {
         cardIndex = 0;
@@ -27,25 +29,35 @@ function randomCardIndex() {
     cardIndex = getRandomInt(0, maxIndex);
 }
 function flipCard() {
-    $("#card").fadeOut(400, function() {
-        $("#card").removeClass("dark");
-        document.getElementById("question").innerHTML = qs[cardIndex]["a"];
-        var url = qs[cardIndex]["url"];
-        document.getElementById("link").setAttribute("href",url);
-        document.getElementById("link").style.visibility = "visible";
-        document.getElementById("think").style.visibility = "visible";
-        $("#card").fadeIn(1000);
-    });
+    if (isFlipped == 0) {
+        $("#card").fadeOut(400, function() {
+            isFlipped = 1;
+            $("#card").removeClass("c0");
+            $("#card").removeClass("c1");
+            $("#card").removeClass("c2");
+            $("#card").removeClass("c3");
+            document.getElementById("question").innerHTML = qs[cardIndex]["a"];
+            var url = qs[cardIndex]["url"];
+            document.getElementById("link").setAttribute("href",url);
+            document.getElementById("link").style.visibility = "visible";
+            $("#card").fadeIn(1000);
+        });
+    }
+}
+function setCardColor() {
+    var n = getRandomInt(0,3);
+    var classes = ['c0','c1','c2','c3'];
+    $("#card").addClass(classes[n]);
 }
 function displayCard() {
     document.getElementById("link").style.visibility = "hidden";
     $("#card").fadeOut(400, function() {
-        $("#card").addClass("dark");
+        setCardColor();
+        isFlipped = 0;
         document.getElementById("question").innerHTML = qs[cardIndex]["q"];
         $("#card").fadeIn(1000);
     });
 }
-
 function nextCard() {
     advanceCardIndex();
     displayCard();
@@ -59,9 +71,17 @@ function randomCard() {
     displayCard();
 }
 function init() {
-    cardIndex = 0;
-    document.getElementById("link").style.visibility = "hidden";
-    $("#card").addClass("dark");
-    document.getElementById("question").innerHTML = qs[cardIndex]["q"];
-    $("#card").fadeIn(1000);
+    var jsonUrl = "questionsAndAnswers.json";
+    $.getJSON(jsonUrl, function(jsonObject) {
+        qs = jsonObject;
+        numCards = qs.length;
+        maxIndex = numCards-1;
+        document.getElementById("link").style.visibility = "hidden";
+        setCardColor();
+        document.getElementById("question").innerHTML = qs[cardIndex]["q"];
+        $("#card").fadeIn(1000);
+    });
 }
+
+
+
